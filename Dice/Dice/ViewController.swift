f35030d7de467c1abd54c748e233232f05ca8035
dextermoran <dextermoran@gmail.com>
2017-08-09 20:36:09 -0500

    tacocat (hello world) app

---
+++ Dice/Dice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dice
+//
+//  Created by Dexter Moran on 8/9/17.
+//  Copyright © 2017 Dexter Moran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
