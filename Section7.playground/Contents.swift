e39b704566ddedaf2027303575f0f3b8467f441f
dextermoran <dextermoran@gmail.com>
2017-08-15 20:51:06 -0500

    formal lecture on playgrounds, functions, and control flow

--- Section7.playground/Contents.swift
+++ Section7.playground/Contents.swift
@@ -1,5 +1,25 @@
-//: Playground - noun: a place where people can play
 
-import Cocoa
+/*
+ 
+ Multi line comment
+ 
+*/
 
-var str = "Hello, playground"
+func nameOfFunction(x : Int) -> Int {
+    print("Functions")
+    print(x)
+    return x * 2
+}
+
+for i in 1...10 {
+    nameOfFunction(x : i)
+}
+
+var Y = nameOfFunction(x: 7)
+print(Y)
+
+if Y < 17 {
+    print("less than 17")
+} else {
+    print("more than 17")
+}
